ed92ae815f78ecd1a96b61c911d92503dff64b77
Jared S <jaredjames2020@gmail.com>
2019-09-23 21:46:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+class Dog
+
+  def name=(dog_name)
+    @name_of_dog = dog_name
+  end
+  
+  def name
+    @name_of_dog
+  end
+  
+  def bark=(dog_bark)
+    @barking_dog = dog_bark
+  end
+  
+  def bark
+    @barking_dog
+    puts "woof!"
+  end
+  
+end
+
+fido = Dog.new
+fido.name = "Fido"
+
+fido.name
+
+fido.bark
