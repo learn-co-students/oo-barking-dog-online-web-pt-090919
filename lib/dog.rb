806a39cb2983163d92504fd445d15721d2a880ed
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-25 22:34:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Your code goes here!
+# Create a Dog class
+# Every instance of a Dog should have a name and be able to bark
+
+class Dog # define a class - Dog
+  
+  #setter method (.name=)
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  
+  #getter method (.name)
+  def name
+    @this_dogs_name
+  end
+  
+  #m ethod that puts "woof!" when called on an instance of Dog
+  def bark 
+    puts "woof!"
+  end
+  
+end 
+
+
+fido = Dog.new
+fido.name = "Fido"
+ 
+fido.name
+# => "Fido"
+ 
+fido.bark
+# woof!
+# => nil
