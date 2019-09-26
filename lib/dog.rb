74039334f0d7995195c5af559c3ab427644f480b
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-26 04:45:29 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Dog
+  attr_accessor :name
+
+  def bark
+    puts "woof!"
+  end
+end
+#class Dog 
+  #def name=(setter)
+#    this_dog_name = setter
+ # end
+ # def name=(getter)
+  #  this_dog_name = getter
+#end
+
+#def bark 
+#  puts "woof!"
+#end
+#end
+# not sure why this worked
